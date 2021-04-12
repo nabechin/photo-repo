9e54fde16c7233097bb41a07ff2d8bf6f3e8c11e
nabechin <nabenabe0258@gmail.com>
2021-04-12 23:34:00 +0900

    install tailwind

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,14 @@
 import React from "react";
 import "./App.css";
+import camera from "./assets/picture/IMG_2122.JPG";
 
 function App(): JSX.Element {
-  return <div>hello world</div>;
+  return (
+    <div>
+      hello world
+      <img src={camera} width="200px" height="200px"></img>
+    </div>
+  );
 }
 
 export default App;
