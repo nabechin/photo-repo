3fcda1acfb1fec92684b748645ad8227c0a56d10
nabechin <nabenabe0258@gmail.com>
2021-04-13 23:33:34 +0900

    feat: style container

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,33 @@
 import React from "react";
 import "./App.css";
+import styled from "styled-components";
 import camera from "./assets/picture/IMG_2122.JPG";
+
+const Header = styled.div`
+  flex: auto;
+  background-color: gray;
+`;
+
+const Wrapper = styled.div`
+  flex: auto 0 1;
+  display: flex;
+  flex-flow: nowrap column;
+  box-sizing: border-box;
+  width: 100%;
+  height: 100%;
+`;
+
+const Footer = styled.div`
+  height: 0;
+  overflow: hidden;
+`;
 
 function App(): JSX.Element {
   return (
     <div>
-      hello world
+      <Header />
+      <Wrapper></Wrapper>
+      <Footer />
       <img src={camera} width="200px" height="200px"></img>
     </div>
   );
