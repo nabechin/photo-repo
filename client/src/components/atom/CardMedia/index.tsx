b790d8443f456da62541f526472a484487e2f0dd
nabechin <nabenabe0258@gmail.com>
2021-04-17 01:08:52 +0900

    feat: make card media component

---
+++ client/src/components/atom/CardMedia/index.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+interface Props {
+  src: string;
+}
+
+export const CardMedia = (props: Props): JSX.Element => {
+  return (
+    <>
+      <img src={props.src} alt="Sunset in the mountains" />
+    </>
+  );
+};
