84c378efbabb7e61b7f2d829c161c8cfc15258b5
nabechin <nabenabe0258@gmail.com>
2021-04-17 01:12:13 +0900

    make card content component

---
+++ client/src/components/atom/CardContent/index.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+interface Props {
+  children?: React.ReactNode;
+}
+
+export const CardContent = (props: Props): JSX.Element => {
+  return (
+    <div className="px-6 py-4">
+      <div className="font-bold text-xl">{props.children}</div>
+    </div>
+  );
+};
