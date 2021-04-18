54cbd9fef5bdcb5f05337dc0e43ee6552b7ba5d6
nabechin <nabenabe0258@gmail.com>
2021-04-18 23:22:53 +0900

    feat: modal component

---
+++ client/src/components/atom/Modal/index.tsx
@@ -0,0 +1,20 @@
+import React, { useEffect } from "react";
+import ReactDom from "react-dom";
+
+const modalRoot = document.getElementById("modal") as HTMLElement;
+
+interface Props {
+  children: React.ReactNode;
+}
+
+export const Modal = (props: Props): React.ReactPortal => {
+  const el = document.createElement("div");
+  useEffect(() => {
+    modalRoot.appendChild(el);
+    return () => {
+      modalRoot.removeChild(el);
+    };
+  });
+
+  return ReactDom.createPortal(props.children, el);
+};
