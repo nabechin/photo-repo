5dd7e6eb9cd0e2a3ac5bc2cdf92e1353a5d7e16e
nabechin <nabenabe0258@gmail.com>
2021-04-19 22:16:14 +0900

    feat: button onclick event

--- client/src/components/atom/Button/index.tsx
+++ client/src/components/atom/Button/index.tsx
@@ -1,12 +1,16 @@
 import React from "react";
 
 interface Props {
+  onClick?: () => void;
   children: React.ReactNode;
 }
 
 export const Button = (props: Props): JSX.Element => {
   return (
-    <button className="bg-white hover:bg-gray-100 text-gray-500 font-semibold py-2 px-4 border border-gray-300 rounded shadow">
+    <button
+      onClick={props.onClick}
+      className="bg-white hover:bg-gray-100 text-gray-500 font-semibold py-2 px-4 border border-gray-300 rounded shadow"
+    >
       {props.children}
     </button>
   );
