ab28041c994639fbbac554117d3fbade0bd73deb
nabechin <nabenabe0258@gmail.com>
2021-04-19 23:31:33 +0900

    fix: button height

--- client/src/components/atom/Button/index.tsx
+++ client/src/components/atom/Button/index.tsx
@@ -9,7 +9,7 @@ export const Button = (props: Props): JSX.Element => {
   return (
     <button
       onClick={props.onClick}
-      className="bg-white hover:bg-gray-100 text-gray-500 font-semibold py-2 px-4 border border-gray-300 rounded shadow"
+      className="h-10 bg-white hover:bg-gray-100 text-gray-500 font-semibold py-2 px-4 border border-gray-300 rounded shadow"
     >
       {props.children}
     </button>
