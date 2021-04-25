84d342e56cd205b67a63d49d5d1781508ea019b3
nabechin <nabenabe0258@gmail.com>
2021-04-25 23:12:05 +0900

    drag and drop style

--- client/src/components/atom/ImageDrop/index.tsx
+++ client/src/components/atom/ImageDrop/index.tsx
@@ -1,29 +1,47 @@
-import React from "react";
+import React, { useState } from "react";
 import Dropzone from "react-dropzone";
+import styled from "styled-components";
 
 export const ImageDrop = (): JSX.Element => {
-  const onHandleDrop = (acceptedFile: File[]) => {
-    console.log(acceptedFile);
+  const [files, setFiles] = useState<File[]>([]);
+  const onHandleDrop = (acceptedFiles: File[]) => {
+    if (acceptedFiles.length > 0) {
+      setFiles(
+        acceptedFiles.map((file) =>
+          Object.assign(file, {
+            preview: URL.createObjectURL(file),
+          })
+        )
+      );
+    }
   };
+
   return (
     <Dropzone onDrop={onHandleDrop}>
       {({ getRootProps, getInputProps }) => (
-        <div
-          {...getRootProps()}
-          style={{
-            display: "flex",
-            justifyContent: "center",
-            alignItems: "center",
-            height: "100%",
-            width: "50%",
-            border: "solid 2px grey",
-            borderRadius: "5px",
-          }}
-        >
-          <input {...getInputProps()} />
-          <p>Drag drop some files here, or click to select files</p>
-        </div>
+        <Wrapper>
+          <DragAndDropBox {...getRootProps()}>
+            <input {...getInputProps()} />
+            <p>Drag drop some files here, or click to select files</p>
+          </DragAndDropBox>
+          <div style={{ flexGrow: 1 }}>aaa</div>
+        </Wrapper>
       )}
     </Dropzone>
   );
 };
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  height: 100%;
+  width: 100%;
+`;
+
+const DragAndDropBox = styled.div`
+  border: solid 2px grey;
+  border-radius: 5px;
+  text-align: center;
+  flex-grow: 2;
+`;
